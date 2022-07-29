d235d909df3455690ced03072244dfe489bc5fbe
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-07-29 12:10:09 -0400

    commit tree d4fbea4b7417c2bc9c143ad1013ad07012f841e4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  
+  
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
@@ -16,5 +17,7 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
+  
+  
 
 end
